ec859cc9a155c0873c6cf1f56c4259597803c637
Kamil Kot <kaamil.kot@gmail.com>
2016-12-27 15:35:46 +0100

    Storage class added

--- src/main/java/com/meow/App.java
+++ src/main/java/com/meow/App.java
@@ -1,5 +1,6 @@
 package com.meow;
 
+import com.meow.dal.Storage;
 import com.meow.model.Actor;
 import com.meow.model.Movie;
 import com.meow.util.HibernateUtil;
@@ -15,36 +16,43 @@ public class App
 {
     public static void main( String[] args )
     {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-
-        Actor actor1 = new Actor();
-        actor1.setName("Kamil Kot");
-        actor1.setRole("James Bond");
-        Actor actor2 = new Actor();
-        actor2.setName("Meow Meow");
-        actor2.setRole("Batman");
-
-        session.save(actor1);
-        session.save(actor2);
-        ArrayList<Actor> actors = new ArrayList<>();
-        actors.add(actor1);
-        actors.add(actor2);
-
-        Movie movie = new Movie();
-        movie.setTitle("Meow");
-        movie.setGenre("Drama");
-        movie.setReleaseYear(1939);
-        movie.setActors(actors);
-        session.save(movie);
-
-
-        long id = 1;
-        Movie movie1 = session.load(Movie.class, id );
-        for (Actor actortemp : movie1.getActors()) {
-            System.out.println(actortemp.getName() + " " + actortemp.getRole());
-        }
-        session.getTransaction().commit();
+        Actor user = new Actor();
+        Storage storage = new Storage(user);
+        storage.beginTransaction();
+        user.setName("Kamil");
+        user.setRole("Bond");
+        storage.insert(user);
+        storage.commit();
+//        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+//        session.beginTransaction();
+//
+//        Actor actor1 = new Actor();
+//        actor1.setName("Kamil Kot");
+//        actor1.setRole("James Bond");
+//        Actor actor2 = new Actor();
+//        actor2.setName("Meow Meow");
+//        actor2.setRole("Batman");
+//
+//        session.save(actor1);
+//        session.save(actor2);
+//        ArrayList<Actor> actors = new ArrayList<>();
+//        actors.add(actor1);
+//        actors.add(actor2);
+//
+//        Movie movie = new Movie();
+//        movie.setTitle("Meow");
+//        movie.setGenre("Drama");
+//        movie.setReleaseYear(1939);
+//        movie.setActors(actors);
+//        session.save(movie);
+//
+//
+//        long id = 1;
+//        Movie movie1 = session.load(Movie.class, id );
+//        for (Actor actortemp : movie1.getActors()) {
+//            System.out.println(actortemp.getName() + " " + actortemp.getRole());
+//        }
+//        session.getTransaction().commit();
 
     }
 }
