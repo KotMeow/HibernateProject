d545195c3605da80af9b513743b65207b30103eb
Kamil Kot <kaamil.kot@gmail.com>
2016-12-29 16:50:27 +0100

    Delete tests added

--- src/test/java/com/meow/MovieDAOTest.java
+++ src/test/java/com/meow/MovieDAOTest.java
@@ -43,11 +43,49 @@ public class MovieDAOTest {
     @Transactional
     @Rollback
     public void testAddActor() {
-       Actor actor = new Actor();
-       actor.setRole("Batman");
-       actor.setName("Kamil Kot");
+        Actor actor = new Actor();
+        actor.setRole("Batman");
+        actor.setName("Kamil Kot");
         actorDAO.addActor(actor);
         List<Actor> actors = actorDAO.getAllActors();
         Assert.assertEquals(actor.getName(), actors.get(0).getName());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testDeleteMovie() {
+        Movie movie1 = new Movie();
+        movie1.setTitle("Batman");
+        movie1.setGenre("Fantasy");
+        Movie movie2 = new Movie();
+        movie2.setTitle("Inception");
+        movie2.setGenre("Drama");
+        movieDAO.addMovie(movie1);
+        movieDAO.addMovie(movie2);
+        Assert.assertEquals(movieDAO.getAllMovies().size(), 2);
+        movieDAO.removeMovie(movie2);
+        Assert.assertEquals(movieDAO.getAllMovies().size(), 1);
+        Assert.assertNull(movieDAO.getMovieById((long) 2));
+        Assert.assertEquals(movieDAO.getAllMovies().size(), 1);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testDeleteActor() {
+        Actor actor1 = new Actor();
+        Actor actor2 = new Actor();
+        actor1.setName("Kamil Kot");
+        actor1.setRole("James Bond");
+        actor2.setName("Leonardo");
+        actor2.setRole("Batman");
+        actorDAO.addActor(actor1);
+        actorDAO.addActor(actor2);
+        Assert.assertEquals(actorDAO.getAllActors().size(), 2);
+        actorDAO.removeActor(actor2);
+        Assert.assertEquals(actorDAO.getAllActors().size(), 1);
+        Assert.assertNull(actorDAO.getActorById((long) 2));
+        Assert.assertEquals(actorDAO.getAllActors().size(), 1);
     }
 }
