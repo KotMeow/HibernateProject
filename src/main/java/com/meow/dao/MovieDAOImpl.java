bf73ff1ea481aab34cbe006f3c2fc6afc02d8fd7
Kamil Kot <kaamil.kot@gmail.com>
2017-01-01 16:03:02 +0100

    HSQL support added

--- src/main/java/com/meow/dao/MovieDAOImpl.java
+++ src/main/java/com/meow/dao/MovieDAOImpl.java
@@ -2,17 +2,12 @@ package com.meow.dao;
 
 import com.meow.model.Actor;
 import com.meow.model.Movie;
-import org.hibernate.Criteria;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
